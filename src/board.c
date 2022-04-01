af360c3a35d18a57e33985e3a8adb9fb69bc8b96
lordofwizard <advaitpandharpurkar619@gmail.com>
2022-04-01 10:30:22 +0530

    converted print function to display character instead of an actual no

--- src/board.c
+++ src/board.c
@@ -6,9 +6,9 @@
 
 void print_board(struct Board * board){
         printf("\n");
-        printf("| %d | %d | %d |\n", board->board[0][0] , board->board[0][1],board->board[0][2]);
-        printf("| %d | %d | %d |\n", board->board[1][0] , board->board[1][1],board->board[1][2]);
-        printf("| %d | %d | %d |\n", board->board[2][0] , board->board[2][1],board->board[2][2]);
+        printf("| %c | %c | %c |\n", board->board[0][0] , board->board[0][1],board->board[0][2]);
+        printf("| %c | %c | %c |\n", board->board[1][0] , board->board[1][1],board->board[1][2]);
+        printf("| %c | %c | %c |\n", board->board[2][0] , board->board[2][1],board->board[2][2]);
         printf("\n");
 }
 void put_o(struct Board * board,int x, int y){
