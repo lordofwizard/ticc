9039e638eebf34ece2a6dd32089e59f40940b2b8
lordofwizard <advaitpandharpurkar619@gmail.com>
2022-04-01 09:54:39 +0530

    Created a printboard method and assigned the all valyues to 69

--- Makefile
+++ Makefile
@@ -5,6 +5,7 @@ SRC_DIRS ?= ./src
 SRCS := $(shell find $(SRC_DIRS) -name *.cpp -or -name *.c -or -name *.s)
 OBJS := $(addsuffix .o,$(basename $(SRCS)))
 DEPS := $(OBJS:.o=.d)
+CC=gcc
 
 INC_DIRS := $(shell find $(SRC_DIRS) -type d)
 INC_FLAGS := $(addprefix -I,$(INC_DIRS))
--- src/main.c
+++ src/main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include "board.h"
 
 int main(){
 	printf("Hello Ticc");
+        struct Board b;
+        for(int i = 0; i < 3; i ++){
+                for(int j = 0 ; j < 3 ; j++)
+                        b.board[i][j]=69;
+        }
+        print_board(&b);
 
-	return 0;        
+        return 0;        
 }
